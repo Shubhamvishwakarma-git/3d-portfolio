88bc6e4661020a975ae3529cf94934818f6eae9b
Shubham Kumar vishwakarma <sv79132@gmail.com>
2025-08-21 20:06:51 +0530

    Fix: corrected import case issue for Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from "./sections/Hero";
 import About from "./sections/About";
 import TechStack from "./sections/TechStack";
 import Projects from "./sections/Projects";
-import Contact from "./sections/contact";
+import Contact from "./sections/Contact";
 import Footer from "./components/Footer";
 import Resume from "./sections/Resume";
 
